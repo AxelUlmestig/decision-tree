111aa30a2b4de1becfa1f1675fee56639254b35a
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-12-30 19:27:26 +0100

    added simple test

---
+++ test/maintest.js
@@ -0,0 +1,42 @@
+const main = require('../main.js');
+const parseTree = require('../parsetree.js').parseTree;
+
+const data = [
+    {
+        value: 2.1,
+        param: 'ost',
+        class: 'A'
+    },
+    {
+        value: 2.1,
+        param: 'ost',
+        class: 'A'
+    },
+    {
+        value: 3,
+        param: 'ost',
+        class: 'B'
+    },
+    {
+        value: 4,
+        param: 'kex',
+        class: 'C'
+    },
+    {
+        value: 4,
+        param: 'kex',
+        class: 'C'
+    },
+    {
+        value: 4,
+        param: 'ost',
+        class: 'D'
+    }
+]
+
+const model = main.train(data, x => x.class);
+console.log(model);
+console.log('training done');
+console.log(parseTree(model)({
+    value: 4
+}));
