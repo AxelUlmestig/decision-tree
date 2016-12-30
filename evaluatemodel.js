be186a98efc0561a1d45399a54c6a8b34031092e
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-12-30 22:33:41 +0100

    added avgError and cleaned up a bit

--- evaluatemodel.js
+++ evaluatemodel.js
@@ -4,18 +4,34 @@ const readFile = require('./readfile.js');
 const evaluateModel = (model, extract, data) => {
     var guesses = 0;
     var correctGuesses = 0;
+    var totalError = 0;
     data.forEach(row => {
-        const prediction = model(row);
-        if(prediction.output == extract(row)) {
+        const prediction = model(row).output;
+        const actual = extract(row);
+        /*
+        console.log('prediction: ', prediction);
+        console.log('actual: ', actual);
+        console.log('');
+        */
+        if(prediction == actual) {
             correctGuesses++;
         }
+        totalError += Math.abs(prediction - actual)
         guesses++;
     });
-    console.log('model accuracy is: ', correctGuesses / guesses);
+    return {
+        accuracy: correctGuesses / guesses,
+        avgError: totalError / guesses
+    };
+    console.log('The model accuracy is: ', correctGuesses / guesses);
+    console.log('The average error is: ', totalError / guesses);
 }
 
 const modelPath = './test/abalone_model.json';
 const model = parseTree(JSON.parse(require(modelPath)));
 
 const dataPath = 'datasets/abalone/abalone_evaluate.csv';
-readFile(dataPath, data => evaluateModel(model, x => x.Rings, data));
+readFile(dataPath, data => {
+    const result = evaluateModel(model, x => x.Rings, data);
+    console.log(result);
+});
