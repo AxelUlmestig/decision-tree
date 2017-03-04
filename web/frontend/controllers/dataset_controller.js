ff44f076da65e139e735a645738f3e9bbabb35ad
axel.ulmestig@gmail.com <axel.ulmestig@gmail.com>
2017-03-04 13:06:51 +0100

    calling for updated dataset metadata after uploading dataset

--- web/frontend/controllers/dataset_controller.js
+++ web/frontend/controllers/dataset_controller.js
@@ -1,31 +1,41 @@
-app.controller('dataset_controller', ($scope, state) => {
+app.controller('dataset_controller', ($scope, $http, state) => {
+    const putDataset = (dataset, name) =>
+        $http({
+            method: 'PUT',
+            url: '/api/dataset/' + name,
+            data: {
+                'filename': name,
+                'dataset': dataset
+            },
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+    const getDatasets = () =>
+        $http({
+            method: 'GET',
+            url: '/api/dataset/',
+        });
+
+    //const updateDatasets = datasets => state.datasets = datasets;
+
     $scope.upload = () => {
         const files = document.getElementById('dataset_file').files;
-        if (files.length <= 0) return false
+        if (files.length <= 0) return false;
 
         const fr = new FileReader();
 
         fr.onload = e => {
-            const result = JSON.parse(e.target.result);
-            const formatted = JSON.stringify(result, null, 2);
-            sendDataset(files[0].name, result)
-        }
+            const filename = files[0].name;
+            const setname = filename.match(/(.*).json$/)[1];
+            const dataset = JSON.parse(e.target.result);
+
+            putDataset(dataset, setname, console.log)
+            .then(getDatasets, console.error)
+            .then(console.log, console.log);
+            //.then(updateDatasets, console.error);
+        };
         fr.readAsText(files[0]);
-    }
+    };
 });
-
-sendDataset = (filename, dataset) => {
-    const http = new XMLHttpRequest();
-    const setname = filename.match(/(.*).json$/)[1];
-    const url = "/api/dataset/" + setname;
-    http.open("PUT", url, true);
-
-    http.setRequestHeader("Content-type", "application/json");
-
-    http.onreadystatechange = () => {
-        if(http.readyState == 4 && http.status == 201) {
-            console.log('upload successful');
-        }
-    }
-    http.send(JSON.stringify({'filename': filename, 'dataset': dataset}));
-}
--- web/frontend/controllers/init.js
+++ web/frontend/controllers/init.js
@@ -1,5 +1,5 @@
 const app = angular.module('decision_tree', []);
 
 app.factory('state', () => ({
-    'dataset_controller': null
+    'datasets': null
 }));
